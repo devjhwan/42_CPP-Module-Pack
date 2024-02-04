1954e1ef5ab01df699df108c69304b993e55eb4e
Junghwan Lee <junghwle@cbr12s5.42barcelona.com>
2024-02-04 18:53:42 +0100

    add more examples on cpp02 ex03

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: junghwle <junghwle@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 00:21:18 by junghwle          #+#    #+#             */
-/*   Updated: 2023/12/24 03:22:12 by junghwle         ###   ########.fr       */
+/*   Updated: 2024/02/04 18:52:39 by junghwle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,11 @@ int	main(void)
 	Point const	v1(0, 0);
 	Point const	v2(5, 10);
 	Point const	v3(10, 0);
-	Point const	p[5] = {Point(5, 5), Point(0, 5), Point(v1), \
-						Point(5, 0), Point(1, 1)};
+	Point const	p[9] = {Point(5, 5), Point(0, 5), Point(v1), \
+						Point(5, 0), Point(1, 1), Point(1, 2), \
+						Point(8,-2), Point(6, 9), Point(9, 1)};
 	std::cout << "Vertex: " << v1 << ", " << v2 << ", " << v2 << std::endl;
-	for (int i = 0; i < 5; i++)
+	for (int i = 0; i < 9; i++)
 	{
 		if (bsp(v1, v2, v3, p[i]))
 			std::cout << "Point " << p[i] << " is inside of triangle" << std::endl;
