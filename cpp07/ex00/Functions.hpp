4c893244a7c45dab57e625beb94c2ac423d2b9e9
devjhwan <devjhwan818@gmail.com>
2024-07-16 20:21:32 +0200

    refactore cpp07 ex00

--- cpp07/ex00/Functions.hpp
+++ cpp07/ex00/Functions.hpp
@@ -2,21 +2,20 @@
 #define FUNCTIONS_HPP
 
 template <typename T>
-class Functions {
-public:
-    static void swap(T &a, T &b) {
-        T tmp = a;
-        a = b;
-        b = tmp;
-    }
+void swap(T &a, T &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
 
-    static T min(const T &a, const T &b) {
-        return (a < b) ? a : b;
-    }
+template <typename T>
+T min(const T &a, const T &b) {
+    return (a < b) ? a : b;
+}
 
-    static T max(const T &a, const T &b) {
-        return (a > b) ? a : b;
-    }
-};
+template <typename T>
+T max(const T &a, const T &b) {
+    return (a > b) ? a : b;
+}
 
 #endif
--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -3,62 +3,46 @@
 #include <string>
 
 template <typename T>
-void	print_values(T &a, T &b) {
-	std::cout << "a = " << a << std::endl;
-	std::cout << "b = " << b << std::endl;
+void testing(T &a, T &b) {
+	std::cout << "Initial values:" << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+	std::cout << std::endl;
+
+	::swap<T>(a, b);
+	std::cout << "Values after swap:" << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+	std::cout << std::endl;
+
+	T _min = ::min<T>(a, b);
+	std::cout << "Min value:" << std::endl;
+	std::cout << "min = " << _min << std::endl;
+	std::cout << std::endl;
+
+	T _max = ::max<T>(a, b);
+	std::cout << "Max value:" << std::endl;
+	std::cout << "max = " << _max << std::endl;
+	std::cout << std::endl;
 }
 
-int main(void) {
-	{
-		std::cout << "Integer testing" << std::endl;
-		std::cout << std::endl;
+int main() {
+    {
+        std::cout << "Integer testing" << std::endl;
+        std::cout << std::endl;
 
-		int a = 2;
-		int b = 3;
+        int a = 2;
+        int b = 3;
+        testing<int>(a, b);
+    }
+    {
+        std::cout << "String testing" << std::endl;
+        std::cout << std::endl;
 
-		std::cout << "Initial values:" << std::endl;
-		print_values(a, b);
-		std::cout << std::endl;
+        std::string a = "abcd";
+        std::string b = "qwer";
+		testing <std::string>(a, b);
+    }
 
-		Functions<int>::swap(a, b);
-		std::cout << "Values after swap:" << std::endl;
-		print_values(a, b);
-		std::cout << std::endl;
-
-		int min = Functions<int>::min(a, b);
-		std::cout << "Min value:" << std::endl;
-		std::cout << "min = " << min << std::endl;
-		std::cout << std::endl;
-
-		int max = Functions<int>::max(a, b);
-		std::cout << "Max value:" << std::endl;
-		std::cout << "max = " << max << std::endl;
-		std::cout << std::endl;
-	}
-	{
-		std::cout << "String testing" << std::endl;
-		std::cout << std::endl;
-
-		std::string a = "abcd";
-		std::string b = "qwer";
-
-		std::cout << "Initial values:" << std::endl;
-		print_values(a, b);
-		std::cout << std::endl;
-
-		Functions<std::string>::swap(a, b);
-		std::cout << "Values after swap:" << std::endl;
-		print_values(a, b);
-		std::cout << std::endl;
-
-		std::string min = Functions<std::string>::min(a, b);
-		std::cout << "Min value:" << std::endl;
-		std::cout << "min = " << min << std::endl;
-		std::cout << std::endl;
-
-		std::string max = Functions<std::string>::max(a, b);
-		std::cout << "Max value:" << std::endl;
-		std::cout << "max = " << max << std::endl;
-		std::cout << std::endl;
-	}
+    return 0;
 }
